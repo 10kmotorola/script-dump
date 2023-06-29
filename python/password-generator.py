9e973a4ed7ad7796adede301f964a0407e4d11e1
Orchid <137848811+mariospicross@users.noreply.github.com>
2023-06-29 04:40:11 -0400

    Update password-generator.py to v1.1.4

--- python/password-generator.py
+++ python/password-generator.py
@@ -3,11 +3,11 @@ import secrets
 import pyperclip
 
 # Variables
-simple = string.ascii_letters + string.digits
-complex = simple + string.punctuation
+simp = string.ascii_letters + string.digits
+comp = simp + string.punctuation
 
 # Credits and stuff
-print("password-generator v1.1.3")
+print("password-generator v1.1.4")
 
 # Questions
 while True:
@@ -18,24 +18,25 @@ while True:
         continue
     else:
         break
-type = input("Would you like your password to be (s)imple or (c)omplex? ")
+sc = input("Would you like your password to be (s)imple or (c)omplex? ")
 
 # Password generator
 while True:
-    if type == "s":
-        password = ''.join(secrets.choice(simple) for i in range(size))
+    if sc == "s":
+        password = ''.join(secrets.choice(simp) for i in range(size))
         print(password)
         pyperclip.copy(password)
-    elif type == "c":
-        password = ''.join(secrets.choice(complex) for i in range(size))
+    elif sc == "c":
+        password = ''.join(secrets.choice(comp) for i in range(size))
         print(password)
         pyperclip.copy(password)
     else:
-        type = input('The answer you provided means nothing to my python script headass. Please provide an answer containing "s" or "c". ')
+        sc = input('The answer you provided means nothing to my python script headass. '
+                   'Please provide an answer containing "s" or "c". ')
         continue
     # Regenerate password
     again = input("Copied to clipboard! Generate again? (y/n) ")
     if again == "y":
-        True
+        pass
     else:
         break
