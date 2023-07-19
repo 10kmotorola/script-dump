17a389acd010c6b14d8dac5a667ee834ec3f6a43
Orchid <137848811+mariospicross@users.noreply.github.com>
2023-07-18 22:36:36 -0400

    Added hex-converter v1.0.0

---
+++ python/hex-converter.py
@@ -0,0 +1,29 @@
+import sys
+import getopt
+import ast
+
+try:
+    # Options
+    options, arguments = getopt.getopt(sys.argv[1:], "id:h:", ["info", "decimal=", "hex="])
+    # Hex converter
+    for currentArgument, currentValue in options:
+        # Displays help information
+        if currentArgument in ("-i", "--info"):
+            print("hex-converter v1.0.0\n-i, --info    = how u got here!\n"
+                  "-d, --decimal = decimal input\n"
+                  "-h, --hex     = hex input")
+        # Converts decimal to hex
+        elif currentArgument in ("-d", "--decimal"):
+            dec_value = int(currentValue)
+            print(hex(dec_value))
+        # Converts hex to decimal
+        elif currentArgument in ("-h", "--hex"):
+            print(ast.literal_eval(currentValue))
+# Prints errors
+except getopt.error as error:
+    print(str(error))
+    print("Please read --info.")
+except ValueError:
+    print("Please provide a value in the form of an integer.")
+except SyntaxError:
+    print("Please provide a value in hexadecimal form.")
